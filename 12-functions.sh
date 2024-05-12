b3c301acf3e38ecc677bd4ad91aa497c61582ade
Venkat <reddy738@gmail.com>
2024-05-12 21:16:15 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -4,14 +4,17 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H:%M:%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
@@ -28,3 +31,6 @@ VALIDATE $? "Installing MySQL"
 
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
+
+dnf install dockerr -y $>>$LOGFILE
+VALIDATE $? "Installing Docker"
