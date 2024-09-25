9f66bb6f8c56f1871ad44d0a8bb5893c2085e086
Venkat <reddy738@gmail.com>
2024-09-25 22:05:08 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#declaring the variables
+PERSON1=SATISH
+PERSON2=VENKAT
+
+echo "$PERSON1 :: How are you $PERSON2 ?"
+echo "$PERSON2 :: I am doing good $PERSON1" 
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#variables from prompt
+echo "Please enter the username::"
+read -s USERNAME
+
+echo "Pleae eneter the password"
+read -s PASSWORD
+
+echo " The given username is :: $USERNAME and the password:: $PASSWORD"
