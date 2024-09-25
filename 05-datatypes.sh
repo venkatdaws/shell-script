c40c3d43320572b07cfbae89b922019e0ec914a7
Venkat <reddy738@gmail.com>
2024-09-25 23:09:24 +0530

    array

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -1,7 +1,13 @@
 #!bin/bash
+#NO1=$1
+#NO2=$2
+
+#SUM=$(($NO1+$NO2))
+#SUM=$(($NO1+$NO2))
+#echo "Total of $NO1 and $NO2 is :$SUM"
 NO1=$1
 NO2=$2
 
-#SUM=$(($NO1+$NO2))
 SUM=$(($NO1+$NO2))
-echo "Total of $NO1 and $NO2 is :$SUM"
+
+echo "Total of $NO1 and $NO2 is: $SUM"
