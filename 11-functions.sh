3dfd9306d0b06fe50764ffbf4b25845292646555
Venkat <reddy738@gmail.com>
2025-10-26 16:18:02 +0530

    loops

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo " $2 .... FAILURE"
+        exit 1
+    else
+        echo "$2 ... Success"
+}
+
+if [$USERID -ne 0]
+then
+    echo "pls run as super user"
+    exit 1
+else
+    echo "You are a super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
