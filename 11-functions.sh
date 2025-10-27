24a1da4ef176154c0a9e0dece826490d945bbcf8
Venkat <reddy738@gmail.com>
2025-10-27 10:18:21 +0530

    loops

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE() {
     if [ $1 -ne 0 ]
