37209ea93b4f2013da8e27ac95429e8d00cb6843
Venkat <reddy738@gmail.com>
2025-10-27 10:49:10 +0530

    loops

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,13 +3,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo " $2 .... FAILURE"
+        echo -e " $2 ....$R FAILURE $N"
         exit 1
     else
-        echo "$2 ... Success"
+        echo -e "$2 ... $R Success $N"
     fi
 }
 
@@ -26,3 +31,6 @@ VALIDATE $? "Installing Mysql"
 
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
+
+dnf install dockerr -y $LOGFILE
+VALIDATE $? "Installing docker.."
