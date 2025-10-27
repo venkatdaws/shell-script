f2a0735abda43be38b5dc33aa90e56c045bd0c0f
Venkat <reddy738@gmail.com>
2025-10-27 15:25:11 +0530

    loops

--- mysql.sh
+++ mysql.sh
@@ -34,7 +34,7 @@ VALIDATE $? "Starting the mysql"
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 #VALIDATE $? "Setting up root password"
 
-mysql -h db.dawss78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.dawss78s.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
