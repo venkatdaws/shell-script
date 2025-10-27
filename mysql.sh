2ddcd4dfe2070107ac7cc74c390d1287bd1faf4f
Venkat <reddy738@gmail.com>
2025-10-27 15:22:08 +0530

    loops

--- mysql.sh
+++ mysql.sh
@@ -31,5 +31,14 @@ VALIDATE $? "Enabling the mysqlid"
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "Starting the mysql"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
+
+mysql -h db.dawss78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
